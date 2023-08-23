9e0a50d22c4c0f2d92a7286f951d937385437170
francesco.miliani <75777914+francescomiliani@users.noreply.github.com>
2023-08-23 13:17:38 +0200

    Add downloaders for ilsole24ore and milanofinanza

---
+++ companies_downloader_from_milanofinanza.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = "https://www.milanofinanza.it/quotazioni/ricerca/aggregazione-238-mf-italy-azioni-ordinarie"
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+    table = soup.find("table")
+
+    if table:
+        rows = table.find_all("tr")
+        data = []
+
+        for row in rows[1:]:  # Skipping the header row
+            cells = row.find_all("td")
+            if len(cells) >= 4:  # Assuming there are at least 4 columns in each row
+                company_name = cells[0].text.strip()
+                codice_isin = cells[len(cells) -1].text.strip()
+                data.append([company_name, codice_isin])
+
+        # Save data to CSV file
+        with open("companies.csv", "w", newline="") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(["Company Name", "Codice ISIN"])  # Write header
+            csvwriter.writerows(data)  # Write data rows
+
+        print("Data saved to companies.csv")
+    else:
+        print("Table not found on the page.")
+else:
+    print("Failed to retrieve the page.")
---
+++ companies_downloader_from_sole24ore.py
@@ -0,0 +1,37 @@
+import requests
+import pandas as pd
+
+base_url = "https://mercati.ilsole24ore.com/azioni/borsa-italiana/ftse-all-share/{}"
+filename = "companies_from_ilsole24ore.csv"
+
+def scrape_page(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        dfs = pd.read_html(response.text)  # Parse HTML tables using pandas
+        if dfs:
+            df = dfs[0]
+            df = df[["Nome", "ISIN", "Categoria"]]  # Extract specific columns
+            return df
+        else:
+            print("Table not found on the page.")
+    else:
+        print("Failed to retrieve the page.")
+
+    return pd.DataFrame()
+
+all_data = []
+page = 1
+while True:
+    url = base_url.format(page)
+    page_data = scrape_page(url)
+    if page_data.empty:
+        break
+    all_data.append(page_data)
+    page += 1
+
+if all_data:
+    combined_data = pd.concat(all_data, ignore_index=True)
+    combined_data.to_csv(filename, index=False)
+    print("Data saved to " + filename)
+else:
+    print("No data found.")
