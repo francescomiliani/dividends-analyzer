74cb4cbd7d7f60d38a2402edcf1dfe9dd1d809d8
francesco.miliani <75777914+francescomiliani@users.noreply.github.com>
2023-08-24 07:19:39 +0200

    Create companies_downloader_from_borsaitaliana.py
    Download companies from Borsa Italiana using html parser because BeautifulSoap didn't work

---
+++ companies_downloader_from_borsaitaliana.py
@@ -0,0 +1,53 @@
+import requests
+from lxml import html
+import re
+import csv
+
+base_url = "https://www.borsaitaliana.it/borsa/azioni/all-share/lista.html?&page={}"
+filename = 'companies.csv'
+
+def extract_isin(href):
+    isin_match = re.search(r"/scheda/(\w+)\.html", href)
+    return isin_match.group(1) if isin_match else None
+
+all_data = []
+
+page = 1
+while True:
+    url = base_url.format(page)
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        tree = html.fromstring(response.content)
+        table = tree.xpath("//table")[1]
+        
+        rows = table.xpath(".//tr")
+        if len(rows) <= 1:
+            break
+        
+        for row in rows[1:]:
+            columns = row.xpath(".//td")
+            nome_a = columns[1].find(".//a")
+            if nome_a is not None:
+                nome = nome_a.text.strip()
+                href = nome_a.get("href")
+                isin = extract_isin(href)
+                print(nome, isin)
+                
+                if isin:
+                    data_row = [nome, isin]
+                    all_data.append(data_row)
+        
+        page += 1
+    else:
+        print(f"Failed to retrieve the page {page}.")
+        break
+
+if all_data:
+    with open(filename, "w", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["Nome", "ISIN"])  # Write header
+        csvwriter.writerows(all_data)  # Write data rows
+    print("Data saved to companies.csv")
+else:
+    print("No data found.")
