f26f0e60a73b88f0f451a0f9a0e510155415a1f1
francesco.miliani <75777914+francescomiliani@users.noreply.github.com>
2023-08-24 21:54:41 +0200

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -15,12 +15,21 @@ def analyze(_company):
     previous_year = None
     init = True
 
-    with open('dataset/' + _company["Ticker"] + '.csv', mode='r') as csv_file:
+    filename = 'dataset/' + _company["Ticker"] + '.csv'
+    with open(filename, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
+        df=pd.read_csv(filename)
+        #FINDING MAX AND MIN
+        max_dividend=df['Dividends'].max()
+        min_dividend=df['Dividends'].min()
+        avg_dividend=df['Dividends'].mean()
+        std_dividend=df['Dividends'].std()
+
         for row in csv_reader:
             input_date = datetime.strptime(row["Date"], '%Y-%m-%d %H:%M:%S%z')
             current_year = input_date.year
-            # Skip the current year because not all companies have yet detached the dividend in the current year
+            # Skip the current year because not all companies have yet detached 
+            # the dividend in the current year
             if current_year == datetime.now().year:
                 break
             if init:
@@ -33,8 +42,9 @@ def analyze(_company):
                 current_yield_strike += 1
                 if current_yield_strike > longest_yield_strike:
                     longest_yield_strike = current_yield_strike
-            elif (current_year - previous_year) > 0:  # subsequent years
+            elif (current_year - previous_year) > 1:  # NOT subsequent years, reset
                 current_yield_strike = 0
+            # if 0 means multiple year with same value e.g 01/2022 05/2022 ecc
             if (current_year - previous_year) != 0:  # do not count equal years e.g. 2000 and 2000
                 yield_counter += 1
             previous_year = current_year
@@ -47,8 +57,10 @@ def analyze(_company):
     if company_info["dividendYield"] != 0:
         dividend_yield = float(company_info["dividendYield"]) * 100
     else:
+        print( 'dividendYield is 0 or N/A => computed ')
         if float(company_info["lastPriceWithDividend"]) != 0:
             dividend_yield = float(company_info["lastDividendValue"])/float(company_info["lastPriceWithDividend"]) * 100
+            print( '==> ', dividend_yield)
         # else: dividend_yield = 0 but it's already set to 0
 
     yieldRatio = (yield_counter / year_time_period * 100) if (year_time_period > 0 ) else 0
@@ -60,13 +72,18 @@ def analyze(_company):
     _company["yearTimePeriod"] = year_time_period
     _company["currentPrice"] = float(company_info["currentPrice"])
     _company["currency"] = company_info["currency"]
-    _company["dividendYieldAPI"] = float(_company["dividendYield"]) # Save the original dividendYield
+    # _company["dividendYieldAPI"] = float(_company["dividendYield"]) # Save the original dividendYield
     _company["dividendYield"] = round(dividend_yield, 3) # new dividendYield
+    _company["minDividend"] = min_dividend
+    _company["maxDividend"] = max_dividend
+    _company["avgDividend"] = round(avg_dividend,3)
+    _company["stdDividend"] = round(std_dividend,3)
 
     print(f'\t\t Yield ratio: {yield_ratio_str} ({_company["yieldRatio"]} %) - '
           f'Longest yield strike: {longest_yield_strike_ratio_str} - '
           f'Current Price: {_company["currentPrice"]} {_company["currency"]} - '
           f'Dividend Yield: {dividend_yield} %'
+          f'Dividend Yield API: {float(_company["dividendYield"]) * 100} %'
           )
     return _company
 
@@ -99,12 +116,16 @@ for i in range(len(company_array)):
 
 # yieldRatio and yearTimePeriod in reverse order
 # currentPrice in natural order
-ordered_list = sorted(company_array, key=lambda d: ( d["yieldRatio"], d["yearTimePeriod"], d["dividendYield"], -d["currentPrice"]), reverse=True)
+ordered_list = sorted(company_array, key=lambda d: ( d["yieldRatio"], d["yearTimePeriod"],
+                                                    d["dividendYield"], -d["currentPrice"]), reverse=True)
 
 with open('output/analysis.csv', mode='w', newline='') as csv_file:
-    fieldnames = ['Company', 'ISIN', 'Ticker', 'Sector', 'currency', 'currentPrice', 'dividendRate', 'dividendYield',
-                  'dividendYieldAPI', 'longestYieldStrike', 'yearTimePeriod', 'yieldCounter', 'yieldRatio',
-                  'firstDividendDate', 'lastDividendDate', 'lastDividendValue', 'lastPriceWithDividend']
+    fieldnames = ['Company', 'ISIN', 'Ticker', 'Sector', 'currency', 
+                  'currentPrice', 'dividendRate', 'dividendYield', 'yieldRatio', 
+                  'longestYieldStrike', 'yieldCounter', 'yearTimePeriod',
+                  'firstDividendDate', 'lastDividendDate', 'lastDividendValue', 
+                  'lastPriceWithDividend', 'minDividend', 'maxDividend', 
+                  'avgDividend', 'stdDividend']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerows(ordered_list)
